1d17c709099ff7381b5db8634b1d4cc601414cc1
KIM DONGHEE <hee.sincostan@gmail.com>
2023-10-15 14:11:19 +0900

    Update disablef12.js

--- assets/js/etc/disablef12.js
+++ assets/js/etc/disablef12.js
@@ -1,8 +1,18 @@
-$(function () {
-    $(document).bind("keydown", function (e) {
-      if (e.keyCode == 123) {
-        e.preventDefault();
-        e.returnValue = false;
+// F12 버튼 방지
+$(document).ready(function(){
+  $(document).bind('keydown',function(e){
+      if ( e.keyCode == 123 /* F12 */) {
+          e.preventDefault();
+          e.returnValue = false;
       }
-    });
   });
+});
+
+// 우측 클릭 방지
+document.onmousedown=disableclick;
+
+function disableclick(event){
+  if (event.button==2) {
+      return false;
+  }
+}
